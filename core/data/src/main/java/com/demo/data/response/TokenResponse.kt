78499bd66819a6ccdd2d6e50b446d45c839f4e0f
AndroidWiz <rashedc004@gmail.com>
2024-12-27 19:16:59 +0600

    session service for token management and logout operations

---
+++ core/data/src/main/java/com/demo/data/response/TokenResponse.kt
@@ -0,0 +1,8 @@
+package com.demo.data.response
+
+import com.google.gson.annotations.SerializedName
+
+class TokenResponse(
+    @SerializedName("accessToken") val accessToken: String,
+    @SerializedName("refreshToken") val refreshToken: String,
+)
