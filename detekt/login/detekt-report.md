a268206f6ace631955f89c3b66064aab96c4af2b
AndroidWiz <rashedc004@gmail.com>
2025-01-06 15:00:20 +0600

    integrating viewmodel for login screen

--- detekt/login/detekt-report.md
+++ detekt/login/detekt-report.md
@@ -2,9 +2,9 @@
 
 ## Metrics
 
-* 18 number of properties
+* 16 number of properties
 
-* 35 number of functions
+* 36 number of functions
 
 * 21 number of classes
 
@@ -14,26 +14,26 @@
 
 ## Complexity Report
 
-* 487 lines of code (loc)
+* 533 lines of code (loc)
 
-* 382 source lines of code (sloc)
+* 425 source lines of code (sloc)
 
-* 218 logical lines of code (lloc)
+* 252 logical lines of code (lloc)
 
 * 15 comment lines of code (cloc)
 
-* 53 cyclomatic complexity (mcc)
+* 55 cyclomatic complexity (mcc)
 
-* 7 cognitive complexity
+* 8 cognitive complexity
 
 * 0 number of total code smells
 
 * 3% comment source ratio
 
-* 243 mcc per 1,000 lloc
+* 218 mcc per 1,000 lloc
 
 * 0 code smells per 1,000 lloc
 
 ## Findings (0)
 
-generated with [detekt version 1.23.3](https://detekt.dev/) on 2025-01-04 11:53:23 UTC
+generated with [detekt version 1.23.3](https://detekt.dev/) on 2025-01-06 08:53:01 UTC
--- features/login/src/main/java/com/demo/login/presentation/view/LoginView.kt
+++ features/login/src/main/java/com/demo/login/presentation/view/LoginView.kt
@@ -2,27 +2,48 @@ package com.demo.login.presentation.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import com.demo.login.R
+import com.demo.login.presentation.flow.LoginInput
+import com.demo.login.presentation.flow.LoginOutput
+import com.demo.login.presentation.flow.LoginViewState
 import com.demo.login.presentation.viewmodel.LoginViewModel
 
 @Composable
-fun LoginScreen(modifier: Modifier = Modifier, loginViewModel: LoginViewModel) {
-  var userName by remember { mutableStateOf("") }
-  var password by remember { mutableStateOf("") }
+fun LoginScreen(
+  modifier: Modifier = Modifier,
+  loginViewModel: LoginViewModel,
+  loginViewState: LoginViewState = LoginViewState(),
+) {
+
+  // react to view output events
+  LaunchedEffect(loginViewModel) {
+    loginViewModel.viewOutput.collect{output ->
+      when(output){
+        is LoginOutput.NavigateToHome -> TODO()
+        is LoginOutput.NavigateToRegister -> TODO()
+        is LoginOutput.ShowError -> TODO()
+      }
+    }
+  }
+
 
   Surface(modifier = modifier.fillMaxSize()) {
     Column(
@@ -31,24 +52,28 @@ fun LoginScreen(modifier: Modifier = Modifier, loginViewModel: LoginViewModel) {
       verticalArrangement = Arrangement.Center,
     ) {
       // user name
-      OutlinedTextField(
-        value = userName,
-        onValueChange = { userName = it },
-        label = { Text(text = "User Name") },
-        modifier = modifier
-          .fillMaxWidth()
-          .padding(8.dp),
-      )
+      CustomTextField(
+        modifier = modifier,
+        label = stringResource(id = R.string.username_label),
+        value = loginViewState.userName,
+        errorMessage = stringResource(id = loginViewState.userNameError.getErrorMessage()),
+        showError = loginViewState.showUsernameError(),
+      ) { username ->
+        loginViewModel.setInput(LoginInput.UsernameUpdated(userName = username))
+      }
+      Spacer(modifier.height(16.dp))
 
       // password
-      OutlinedTextField(
-        value = password,
-        onValueChange = { password = it },
-        label = { Text(text = "Password") },
-        modifier = modifier
-          .fillMaxWidth()
-          .padding(8.dp),
-      )
+      CustomTextField(
+        modifier = modifier,
+        label = stringResource(id = R.string.password_label),
+        value = loginViewState.password,
+        errorMessage = stringResource(id = loginViewState.passwordError.getErrorMessage()),
+        showError = loginViewState.showPasswordError(),
+      ) { password ->
+        loginViewModel.setInput(LoginInput.PasswordUpdated(password = password))
+      }
+      Spacer(modifier.height(16.dp))
 
       // login button
       Button(
@@ -57,6 +82,40 @@ fun LoginScreen(modifier: Modifier = Modifier, loginViewModel: LoginViewModel) {
       ) {
         Text(text = "Login")
       }
+      Spacer(modifier = modifier.height(16.dp))
+
+      TextButton(onClick = { loginViewModel.setInput(LoginInput.RegisterButtonClicked) }) {
+        Text(text = "Register here!")
+      }
     }
+  }
+}
+
+@Composable
+fun CustomTextField(
+  modifier: Modifier,
+  label: String,
+  value: String,
+  showError: Boolean,
+  errorMessage: String,
+  visualTransformation: VisualTransformation = VisualTransformation.None,
+  onChanged: (String) -> Unit,
+) {
+  OutlinedTextField(
+    value = value,
+    onValueChange = { onChanged(it) },
+    label = { Text(text = label) },
+    isError = showError,
+    visualTransformation = visualTransformation,
+    modifier = modifier
+      .fillMaxWidth()
+      .padding(8.dp),
+  )
+  if (showError) {
+    Text(
+      text = errorMessage,
+      color = Color.Red,
+      modifier = modifier.padding(all = 8.dp),
+    )
   }
 }
