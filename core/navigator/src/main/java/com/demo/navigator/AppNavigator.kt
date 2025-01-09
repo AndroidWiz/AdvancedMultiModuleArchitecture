a9c6d92c3379225035300f5e1f950f8413c6142c
AndroidWiz <rashedc004@gmail.com>
2025-01-09 23:01:41 +0600

    custom AppNavigator and NavigatorEvent

---
+++ core/navigator/src/main/java/com/demo/navigator/AppNavigator.kt
@@ -0,0 +1,19 @@
+package com.demo.navigator
+
+import androidx.navigation.NavOptionsBuilder
+import com.demo.navigator.event.NavigatorEvent
+import kotlinx.coroutines.flow.Flow
+
+interface AppNavigator {
+
+    fun navigateUp(): Boolean
+
+    fun popBackStack()
+
+    fun navigateTo(
+        route: String,
+        navOptionsBuilder: NavOptionsBuilder.() -> Unit = { launchSingleTop = true },
+    ): Boolean
+
+    val destinations: Flow<NavigatorEvent>
+}
