28b02ea46191a7812538b67c6bfadb3f504239ce
AndroidWiz <rashedc004@gmail.com>
2025-01-10 18:46:16 +0600

    Typed Navigation Routes using Sealed Classes

---
+++ core/navigator/src/main/java/com/demo/navigator/destinations/Screens.kt
@@ -0,0 +1,11 @@
+package com.demo.navigator.destinations
+
+sealed class Screens(val route: String) {
+
+    data object LoginScreenRoute : Screens(route = LOGIN_SCREEN_ROUTE)
+
+    data object RegistrationScreenRoute : Screens(route = REGISTRATION_SCREEN_ROUTE)
+
+    data object HomeScreenRoute :
+        Screens(route = "$HOME_SCREEN_ROUTE/$USER_PARAMETER/$USER_FULL_NAME/$USER_AGE")
+}
---
+++ core/navigator/src/main/java/com/demo/navigator/di/NavigatorModule.kt
@@ -0,0 +1,14 @@
+package com.demo.navigator.di
+
+import com.demo.navigator.core.AppNavigator
+import com.demo.navigator.core.AppNavigatorImpl
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class NavigatorModule {
+
+    abstract fun navigator(appNavigatorImpl: AppNavigatorImpl): AppNavigator
+}
