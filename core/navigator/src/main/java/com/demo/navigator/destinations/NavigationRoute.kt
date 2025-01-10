c0dfd7c6608280f4103012a662136a23245b271d
AndroidWiz <rashedc004@gmail.com>
2025-01-10 18:01:32 +0600

    route/destination interface

---
+++ core/navigator/src/main/java/com/demo/navigator/destinations/NavigationRoute.kt
@@ -0,0 +1,13 @@
+package com.demo.navigator.destinations
+
+import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavDeepLink
+
+interface NavigationRoute {
+
+    fun route(): String
+
+    val arguments: List<NamedNavArgument> get() = emptyList()
+
+    val deepLinks: List<NavDeepLink> get() = emptyList()
+}
